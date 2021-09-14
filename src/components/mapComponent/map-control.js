2b496e50828c050300e383e00f9dbad09fc74769
Sebastian <novas.lobog@konradlorenz.edu.co>
2021-09-13 23:47:30 -0500

    adicion miniMapa

--- src/components/mapComponent/map-control.js
+++ src/components/mapComponent/map-control.js
@@ -1,18 +1,24 @@
 import 'leaflet/dist/leaflet.css';
 import './map.scss';
 
+
+
 const L =require ('leaflet');
 
 import {carto_light} from './layers/control-layers'
+import {minimap} from './controls/minimap.js'
 
 export var map = L.map('map', {
     center: [5.337628, -72.394055],
-    zoom: 13.8,
+    zoom: 14,
     layers: [carto_light]
 });
 
 L.control.zoom({position: 'topright'}).addTo(map);
+minimap.addTo(map);
+new L.control.scale({imperial: false}).addTo(map)
 
 // scale control
-new L.control.scale({imperial: false}).addTo(map)
+
+
 
